3d8c78f811aeeacbd1bff2d9b0eb99e5a9a53909
Hung Hoang <hunghq.work@gmail.com>
2023-07-15 15:28:56 +0700

    Service resgistry

--- department-service/src/main/java/com/microservice/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/microservice/departmentservice/DepartmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // kích hoạt eureka discoverry client
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
